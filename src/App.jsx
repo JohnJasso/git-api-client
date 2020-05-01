7c0eebe1c86e96332317f75c93abc4997a9c3bd6
John Jasso <johnjasso@Johns-MacBook-Pro.local>
2020-05-01 01:36:38 -0500

    (Gapp-2) Modify Navbar. Delete unused files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Default from "./components/pages/Default";
 
--- src/components/atoms/NavBar.jsx
+++ src/components/atoms/NavBar.jsx
@@ -4,9 +4,9 @@ class NavBar extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark">
-        <span className="navbar-brand mb-0 h1">
+        <span className="navbar-brand mb-0 h1" style={{ marginLeft: 70 }}>
           {this.props.title}
-          <span className="badge badge-pill badge-primary ml-2">
+          <span className="badge badge-pill badge-info ml-3">
             {this.props.totalBookmarks}
           </span>
         </span>
